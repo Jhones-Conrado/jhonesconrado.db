2a9f16433a9b130cc5c8d7b3664d5dea0d361219
Jhones Conrado <jhonesconrado@gmail.com>
2022-04-13 11:49:25 -0300

    limpeza de codigo no webcommunication

--- src/jhonDES/webserver/WebCommunication.java
+++ src/jhonDES/webserver/WebCommunication.java
@@ -23,7 +23,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Date;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jhonDES.template.populate.HTMLManager;
@@ -52,15 +51,9 @@ public class WebCommunication extends Thread{
     }
     
     private String readHeader() throws IOException{
-//        Scanner s = new Scanner(in);
-        
         InputStreamReader isReader = new InputStreamReader(in);
         BufferedReader br = new BufferedReader(isReader);
-        
-        
-//        StringBuilder sb = new StringBuilder();
         String temp = null;
-//        int i = 0;
         
         StringBuilder header = new StringBuilder();
         while((temp = br.readLine()).length() != 0){
@@ -71,47 +64,12 @@ public class WebCommunication extends Thread{
         typeRequest = basics[0];
         path = basics[1];
         
-        System.out.println(header.toString());
-        
         StringBuilder corpo = new StringBuilder();
         if(typeRequest.equals("POST")){
             while(br.ready()){
                 corpo.append((char) br.read());
             }
-            System.out.println(corpo.toString());
         }
-
-        
-        
-//        while(true){
-//            try {
-//                temp = s.nextLine();
-//            } catch (Exception e) {
-//            }
-//            if(i == 0){
-//                String[] basics = temp.split(" ");
-//                typeRequest = basics[0];
-//                path = basics[1];
-//                i = 1;
-//            }
-//            sb.append(temp+"\n");
-//            
-//            if(temp.startsWith("Content-Length: ")){
-//                Content_Length = Integer.valueOf(temp.substring("Content-Length: ".length()));
-//            }
-//            if(temp.isEmpty()){
-//                break;
-//            }
-//            
-//        }
-//        try {
-//            System.out.println("AQUI AAAAAAAAAAAAAAAAAAAAAAAAAA");
-//            byte[] bytes = in.readNBytes(Content_Length);
-//            sb.append(new String(bytes));
-//        } catch (IOException ex) {
-//            Logger.getLogger(WebCommunication.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        System.out.println(sb.toString());
         
         return header.toString()+"\n"+corpo.toString();
     }
